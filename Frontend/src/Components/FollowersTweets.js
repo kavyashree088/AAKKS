bbb5431285b57dabf02aa74055d3d005da1d34e7
Kavyashree <kavyashree088@gmail.com>
2019-11-23 23:16:02 -0800

    push

--- Frontend/src/Components/FollowersTweets.js
+++ Frontend/src/Components/FollowersTweets.js
@@ -1,51 +1,48 @@
 import React, { Component } from "react";
 import { Row, Col, Card } from 'react-bootstrap'
-import axios from "axios";
-import swal from 'sweetalert';
-const settings = require("../config/settings.js");
 
 export class FollowersTweets extends Component {
-   state = {
-       allTweets : []
-   };
-   constructor(props){
-       super(props);
-       this.state.allTweets = this.props.allTweets;
-   }
-   render(){
-       let allTweets = this.state.allTweets;
-       let tweetsMarkup = [];
-       if(allTweets && allTweets.length > 0){
-           for(let i= 0; i< allTweets.length; i++){
-            tweetsMarkup.push(<TweetComponent key={i} tweet = { allTweets[i] }/>);
-           }
-           return tweetsMarkup;
-       } else {
-           return <div></div>;
-       }
-   }
+    state = {
+        allTweets: []
+    };
+    constructor(props) {
+        super(props);
+        this.state.allTweets = this.props.allTweets;
+    }
+    render() {
+        let allTweets = this.state.allTweets;
+        let tweetsMarkup = [];
+        if (allTweets && allTweets.length > 0) {
+            for (let i = 0; i < allTweets.length; i++) {
+                tweetsMarkup.push(<TweetComponent key={i} tweet={allTweets[i]} />);
+            }
+            return tweetsMarkup;
+        } else {
+            return <div></div>;
+        }
+    }
 }
 class TweetComponent extends Component {
     state = {
-        tweetText : '',
-        likesNum : 0,
-        repliesNum : 0
+        tweetText: '',
+        likesNum: 0,
+        repliesNum: 0
     };
-    constructor(props){
+    constructor(props) {
         super(props);
-        if(this.props.tweet){
+        if (this.props.tweet) {
             this.state.tweetText = this.props.tweet.tweetText;
             this.state.likesNum = this.props.tweet.likes ? this.props.tweet.likes.length : 0;
             this.state.repliesNum = this.props.tweet.replies ? this.props.tweet.replies.length : 0;
         }
     }
-    render(){
-        return(
+    render() {
+        return (
             <Card>
                 <div> profilepic </div>
                 <Row><div> {this.state.tweetText} </div> </Row>
                 <Row>
-                    <Col> {this.state.likesNum} likes</Col> 
+                    <Col> {this.state.likesNum} likes</Col>
                     <Col> {this.state.repliesNum} replies</Col>
                 </Row>
             </Card>
