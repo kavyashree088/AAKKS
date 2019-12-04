704b48a6b13493946c35e79dcbc44a45e9e19fa1
Anjali <anjali.abndaru@sjsu.edu>
2019-12-04 01:03:27 -0800

    Added chart changes

--- Frontend/src/config/settings.js
+++ Frontend/src/config/settings.js
@@ -5,7 +5,7 @@ module.exports = {
     "rooturl": "backendLoadbalancer-151734030.us-east-1.elb.amazonaws.com",
     's3bucket': 'https://twitter-g12-bucket.s3.us-east-2.amazonaws.com/',
     'imageurl':'twitter-g12-bucket.s3.us-east-2.amazonaws.com',
-    'frontendHostName': 'rontendloadbalancer-16730124.us-east-2.elb.amazonaws.com',
+    'frontendHostName': 'frontendloadbalancer-16730124.us-east-2.elb.amazonaws.com',
     'frontendPort': 3000,
    
 };
